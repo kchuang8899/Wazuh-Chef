d9b347c7debf4d27c6b6821dc8151b915210199d
Jose Luis Ruiz <jose@wazuh.com>
2016-07-17 13:10:44 +0200

    delete integration

--- test/integration/default/serverspec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require 'common_ossec_tests'
-
-describe 'pm_wazuh_ossec::default' do
-  # This is just a sample integration test
-  describe file('/etc/passwd') do
-    it { should be_file }
-  end
-end
